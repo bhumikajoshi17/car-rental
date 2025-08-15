27c1600b0ac40adf960cd55a5a357bf24eea8190
bhumikajoshi17 <joshibhumi172005@gmail.com>
2025-08-15 15:07:46 +0530

    changeapp-context

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -40,7 +40,7 @@ export const AppProvider=({children})=>{
     // function to fetch all cars from the server
     const fetchCars = async () => {
         try {
-            const { data } = await axios.get('/api/user/cars');
+            const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/api/user/cars`);
             data.success?  setCars(data.cars) : toast.error(data.message);
         } catch (error) {
             toast.error(error.message);
