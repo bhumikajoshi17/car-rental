bf141db45c826170d952b5cbc5f58c1b71acad6c
BHUMIKA JOSHI <joshibhumika555@gmail.com>
2025-08-25 11:57:05 +0000

    comment routes

--- server/Server.js
+++ server/Server.js
@@ -2,9 +2,9 @@ import express from 'express';
 import "dotenv/config" ;
 import cors from "cors";
 import connectDB from './CONFIGS/db.js';
-import userRouter from './Routes/userRoutes.js';
-import ownerRouter from './Routes/ownerRoutes.js';
-import bookingRouter from './Routes/bookingRoutes.js';
+// import userRouter from './Routes/userRoutes.js';
+// import ownerRouter from './Routes/ownerRoutes.js';
+// import bookingRouter from './Routes/bookingRoutes.js';
 
 // Initialize Express App
 
@@ -45,9 +45,9 @@ app.use(express.json());
 console.log('json')
 
 app.get('/' , (req,res)=>res.send("Server is Running!"))
-app.use('/api/user', userRouter)
-app.use("/api/owner" ,ownerRouter)
-app.use("/api/bookings" ,bookingRouter)
+// app.use("/api/user", userRouter)
+// app.use("/api/owner" ,ownerRouter)
+// app.use("/api/bookings" ,bookingRouter)
 
 const PORT = process.env.PORT || 3000;
 
